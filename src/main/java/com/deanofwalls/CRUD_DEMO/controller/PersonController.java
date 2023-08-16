50a1db3563945ee70eb08c86e49191133a2207d7
deanOfWalls <deanofwalls@gmail.com>
2023-08-16 02:02:19 -0400

    Modified CrossOrigin url

--- src/main/java/com/deanofwalls/CRUD_DEMO/controller/PersonController.java
+++ src/main/java/com/deanofwalls/CRUD_DEMO/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "https://github.com/deanOfWalls/CRUD_with_CORS")
+@CrossOrigin(origins = "https://deanofwalls.github.io/CRUD_CORS_Front/")
 @Controller
 public class PersonController {
     private PersonService service;
